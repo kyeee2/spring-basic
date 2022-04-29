2de9dc28b91c063b93da4041397802b162e0eeb6
김영은 <youngeun0208@naver.com>
2022-04-30 01:08:27 +0900

    옵션 처리, 생성자 주입의 중요성, 롬복 사용

--- src/test/java/hello/core/CoreApplicationTests.java
+++ src/test/java/hello/core/CoreApplicationTests.java
@@ -1,10 +1,16 @@
 package hello.core;
 
+import hello.core.member.MemberService;
+import hello.core.member.MemoryMemberRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CoreApplicationTests {
+
+	@Autowired
+	MemberService memberService;
 
 	@Test
 	void contextLoads() {
